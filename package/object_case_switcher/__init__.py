f0560c69fe9d1dec25a09d1b94504669e26f393b
Alex-Andr-19 <andrey.aleksandrychev3@mail.ru>
2024-08-16 13:11:00 +0300

    Add description for module

--- package/object_case_switcher/__init__.py
+++ package/object_case_switcher/__init__.py
@@ -1,3 +1,7 @@
+"""
+You need only one function `transform_structure`
+"""
+
 from .transformFunctions import *
 from .transformPropModule import transform_structure
 from .TransformStructure import *
--- package/setup.py
+++ package/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 
 NAME = "object_case_switcher"
-VERSION = "0.0.7"
+VERSION = "0.0.8"
 
 setup(
     version=VERSION,
