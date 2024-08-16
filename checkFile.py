35f095874b6b48005d081ff018d8d409adbfac6d
Alex-Andr-19 <andrey.aleksandrychev3@mail.ru>
2024-08-16 08:37:27 +0300

    Create checkFile for package

---
+++ checkFile.py
@@ -0,0 +1,11 @@
+# from object_case_switcher import transform_structure
+
+from object_case_switcher import transform_structure
+
+@transform_structure(_async=False, case_type="camelToSnake")
+def some_function():
+    return {
+        "asdgAsdf": 42
+    }
+
+print(some_function())
