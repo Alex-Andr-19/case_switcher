5269f35bfc8f302d1ac410de113b8c7bdd08e77a
Alex-Andr-19 <andrey.aleksandrychev3@mail.ru>
2024-08-16 08:47:15 +0300

    Some change

--- checkFile.py
+++ checkFile.py
@@ -5,7 +5,7 @@ from object_case_switcher import transform_structure
 @transform_structure(_async=False, case_type="camelToSnake")
 def some_function():
     return {
-        "asdgAsdf": 42
+        "asdgAsdf": 42,
     }
 
 print(some_function())
