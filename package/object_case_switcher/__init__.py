c6dc9f93448924de0549170e82599c6f61730aeb
Alex-Andr-19 <andrey.aleksandrychev3@mail.ru>
2024-08-16 14:04:24 +0300

    Try to get back to absence of modeule description

--- package/object_case_switcher/__init__.py
+++ package/object_case_switcher/__init__.py
@@ -1,7 +1,3 @@
-"""
-You need only one function `transform_structure`
-"""
-
 from .transformFunctions import *
 from .transformPropModule import transform_structure
 from .TransformStructure import *
